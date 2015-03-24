8a105b399953242c2db57a215c01533afb79e381
git-team-cs <sagara_st@fieldsystem.co.jp>
2015-03-24 16:08:00 +0900

    終了時トースト表示機能追加

--- repo_test/src/com/example/repo_test/MainActivity.java
+++ repo_test/src/com/example/repo_test/MainActivity.java
@@ -17,6 +17,12 @@ public class MainActivity extends Activity {
 	}
 
 	@Override
+	protected void onDestroy() {
+		Toast.makeText(getApplicationContext(), "終了時トースト表示", Toast.LENGTH_SHORT).show();
+		super.onDestroy();
+	}
+
+	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.main, menu);
